24f39325e50e7e59bb6d80bfead824b31c93bf07
DESKTOP-IK7TDUL\BoaJoo <joocong2265@gmail.com>
2024-02-05 21:01:29 +0900

    게시판 등록 오류 수정 완료

--- joo-board/src/main/webapp/resources/js/detail.js
+++ joo-board/src/main/webapp/resources/js/detail.js
@@ -1,4 +1,48 @@
 $(document).ready(function(){
+// 등록 버튼 클릭
+if(document.getElementById('insert')) {
+	document.getElementById('insert').onclick = function() {
+			console.log("등록버튼 클릭");
+		var formData = new FormData($('#boardForm')[0]);
+		var title = $("#title").val();
+		var content = $("#content").val();
+
+		if (title === "") {
+			alert("제목을 입력하세요.");
+			return;
+		} 
+		if (content === "") {
+			alert("내용을 입력하세요.");
+			return;
+		}
+
+		$.ajax({
+			url: '/api/insert',
+			type: 'POST',
+			async: false,
+			data: formData,
+			contentType:false,
+			processData: false,
+			// async: true,
+			success : function(result) { 
+				// 결과 성공 콜백함수        
+				alert(result.message);  
+				window.location.href = "/api/list";  
+			},
+			error: function (jqXHR, textStatus, errorThrown,error) {
+				console.error('Ajax request failed:', textStatus, errorThrown);
+				console.log('Server response:', jqXHR.responseText);
+				alert('에러 발생: ' + error.responseText);
+			}	
+		});
+		// ajax 끝
+	}
+}
+	
+
+
+
+
 // 수정 버튼 클릭시
 if(document.getElementById('updateWrite')) {
 	document.getElementById('updateWrite').onclick = function() {
